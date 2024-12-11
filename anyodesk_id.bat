d9187cfa8cdd3f2f6d3e99ac535028f43c4cf0d6
swethadhry <swethadhry@users.noreply.github.com>
2024-12-11 21:28:46 +0530

    Update anyodesk_id.bat

--- anyodesk_id.bat
+++ anyodesk_id.bat
@@ -1,3 +1,3 @@
 @echo off
-for /f "delims=" %%i in (C:\ProgramData\AnyDesk\AnyDesk.exe" --get-id') do set ID=%%i
+for /f "delims=" %%i in ('"C:\ProgramData\AnyDesk\AnyDesk.exe" --get-id') do set ID=%%i
 echo AnyDesk ID is: %ID%
